08a4dad686443d76fef090bdcd377ff729cf91b2
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-04-13 01:21:49 +0600

    ok

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.json());
 // user: dbUser2
 // pass : lHLS5R9sDwb6ItpG
 
-
-
 const uri = "mongodb+srv://dbUser2:lHLS5R9sDwb6ItpG@cluster0.hlsud.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -26,22 +24,21 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users)
         });
-        // get user by id
+
         app.get('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const user = await userCollection.findOne(query);
             res.send(user)
-        });
+        })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            console.log(user)
             const result = await userCollection.insertOne(user);
             res.send(result)
         });
 
-        //update
+        //update 
         app.put('/users/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
@@ -58,22 +55,77 @@ async function run() {
             res.send(result)
         })
 
-        // for delete
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
-            // console.log('trying to delete ', id);
             const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
-            console.log(result);
             res.send(result)
-        });
-
+        })
     }
     finally {
 
     }
-};
+}
 run().catch(err => console.log(err));
+
+// async function run() {
+//     try {
+//         const userCollection = client.db('nodeMongoCrud').collection('crudUsers');
+
+//         app.get('/users', async (req, res) => {
+//             const query = {};
+//             const cursor = userCollection.find(query);
+//             const users = await cursor.toArray();
+//             res.send(users)
+//         });
+//         // get user by id
+//         app.get('/users/:id', async (req, res) => {
+//             const id = req.params.id;
+//             const query = { _id: new ObjectId(id) };
+//             const user = await userCollection.findOne(query);
+//             res.send(user)
+//         });
+
+//         app.post('/users', async (req, res) => {
+//             const user = req.body;
+//             console.log(user)
+//             const result = await userCollection.insertOne(user);
+//             res.send(result)
+//         });
+
+//         //update
+//         app.put('/users/:id', async (req, res) => {
+//             const id = req.params.id;
+//             const filter = { _id: new ObjectId(id) };
+//             const user = req.body;
+//             const option = { upsert: true };
+//             const updatedUser = {
+//                 $set: {
+//                     name: user.name,
+//                     address: user.address,
+//                     email: user.email
+//                 }
+//             }
+//             const result = await userCollection.updateOne(filter, updatedUser, option);
+//             res.send(result)
+//         })
+
+//         // for delete
+//         app.delete('/users/:id', async (req, res) => {
+//             const id = req.params.id;
+//             // console.log('trying to delete ', id);
+//             const query = { _id: new ObjectId(id) };
+//             const result = await userCollection.deleteOne(query);
+//             console.log(result);
+//             res.send(result)
+//         });
+
+//     }
+//     finally {
+
+//     }
+// };
+// run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
